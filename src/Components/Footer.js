c177e4aed9fd24a683632209ffeb216d35dfdf65
claceotto <clatimponi@gmail.com>
2024-05-03 22:16:39 +0100

    progress on making footer responsive

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,49 +1,60 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope} from "@fortawesome/free-solid-svg-icons"
-
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 function Footer() {
   return (
     <footer>
-      <div className="footer-logo">
-        <img src="../images/Logo.jpg" alt="Little Lemon Logo" />
+      <div className="bg-footer-logo">
+        <img
+          src="../images/Logo.jpg"
+          alt="Little Lemon Logo"
+          className="bg-logo"
+        />
       </div>
-      {/* <div className="footer-text"> */}
-        <div className="footer-links">
-          <h3>Little Lemon</h3>
-          <a href="/home">Home</a>
-          <a href="/about">About</a>
-          <a href="/menu">Menu</a>
-          <a href="/reservations">Reservations</a>
-          <a href="/order-online">Order Online</a>
-          <a href="/login">Login</a>
-        </div>
-        <div className="footer-contact">
-          <h3>Contact</h3>
-          <p>
-            Limoeiro Avenue
-            <br />
-            Citrus City
-            <br />
-            Isle of Man
-          </p>
-          <p>
-            Telephone:
-            <br />
-            000-0000
-          </p>
-          <p>
-            E-mail:
-            <br />
-            info@little-lemon.com
-          </p>
-        </div>
-        <div className="footer-social">
+
+      <div className="sm-fotter-logo">
+        <img
+          src="../images/Vertical-logo.png"
+          alt="Little Lemon Logo"
+          className="sm-logo"
+        />
+      </div>
+
+      <div className="footer-links">
+        <h3>Little Lemon</h3>
+        <a href="/home">Home</a>
+        <a href="/about">About</a>
+        <a href="/menu">Menu</a>
+        <a href="/reservations">Reservations</a>
+        <a href="/order-online">Order Online</a>
+        <a href="/login">Login</a>
+      </div>
+      <div className="footer-contact">
+        <h3>Contact</h3>
+        <p>
+          Limoeiro Avenue
+          <br />
+          Citrus City
+          <br />
+          Isle of Man
+        </p>
+        <p>
+          Telephone:
+          <br />
+          000-0000
+        </p>
+        <p>
+          E-mail:
+          <br />
+          info@little-lemon.com
+        </p>
+      </div>
+      <div className="footer-social">
         <FontAwesomeIcon className="footer-icon" icon={faFacebook} />
         <FontAwesomeIcon className="footer-icon" icon={faInstagram} />
         <FontAwesomeIcon className="footer-icon" icon={faEnvelope} />
-        </div>
+      </div>
     </footer>
   );
 }
