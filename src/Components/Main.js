7aa4c976edd34a07257efedcef367e4d27a9125d
claceotto <clatimponi@gmail.com>
2024-05-06 18:12:04 +0100

    progress on specials formatting

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -31,34 +31,37 @@ function Main() {
           />
         </div>
       </article>
-
       <article className="specials">
-        <h1>This week's specials</h1>
-        <Button btext={"Online Menu"} />
-        <Card
-          src={"../images/greek salad.jpg"}
-          title={"Greed salad"}
-          price={"£12.99"}
-          description={
-            "The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. "
-          }
-        />
-        <Card
-          src={"../images/Bruchetta.jpg"}
-          title={"Bruschetta"}
-          price={"£5.99"}
-          description={
-            "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil"
-          }
-        />
-        <Card
-          src={"../images/lemon dessert.jpg"}
-          title={"Lemon dessert"}
-          price={"£5.00"}
-          description={
-            "This come straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined."
-          }
-        />
+        <div className="specials-title">
+          <h1>This week's specials</h1>
+          <Button btext={"Online Menu"} />
+        </div>
+        <div className="specials-cards">
+          <Card
+            src={"../images/greek salad.jpg"}
+            title={"Greed salad"}
+            price={"£12.99"}
+            description={
+              "The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. "
+            }
+          />
+          <Card
+            src={"../images/Bruchetta.jpg"}
+            title={"Bruschetta"}
+            price={"£5.99"}
+            description={
+              "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil"
+            }
+          />
+          <Card
+            src={"../images/lemon dessert.jpg"}
+            title={"Lemon dessert"}
+            price={"£5.00"}
+            description={
+              "This come straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined."
+            }
+          />
+        </div>
       </article>
       <article>Testimonials</article>
       <article>About</article>
