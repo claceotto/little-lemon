33cebf3e7f1312e99bcba4571cb70dbc0ee6b321
claceotto <clatimponi@gmail.com>
2024-05-14 17:29:04 +0100

    Started on the about us section

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -92,7 +92,17 @@ function Main() {
           />
         </div>
       </article>
-      <article>About</article>
+      <article className="about">
+        <h1>About us</h1>
+        <div className="about-content"> 
+        <div className="about-text">
+          <p>Based in the Isle of Man, Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. </p>
+          <p>Our chefs draw inspiration from Italian, Greek, and Turkish culture, creating a menu of 12-15 items that we rotate seasonally. </p>
+          <p>The restaurant boasts a rustic and relaxed atmosphere with moderate prices, making it a popular place for a meal any time of the day.</p>
+          </div>
+        <img src="../images/Mario and Adrian b.jpg" alt="A chef and restaurant manager" />
+        </div>
+      </article>
     </main>
   );
 }
