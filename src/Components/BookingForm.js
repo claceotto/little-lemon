84ba3430c091ada11cf640ea4a48ad5e1a2aa1c1
claceotto <clatimponi@gmail.com>
2024-05-20 22:09:22 +0100

    Further work on form fields and style

--- src/Components/BookingForm.js
+++ src/Components/BookingForm.js
@@ -1,9 +1,21 @@
+import "./BookingForm.css"
+import Button from "./Button";
+
 export default function BookingForm() {
   return (
-    <form>
-      <label for="res-date">When?</label>
+    <form className="reservation">
+      <div className="date">
+        <div className="form-title">
+          <span className="circle">1</span>
+          <label for="res-date" className="res-title">When?</label>
+        </div>
       <input type="date" id="res-date" />
-      <label for="res-time">What time?</label>
+      </div>
+      <div className="time">
+      <div className="form-title">
+        <span className="circle">2</span>
+        <label for="res-time" className="res-title">What time?</label>
+      </div>
       <select id="res-time ">
         <option>17:00</option>
         <option>18:00</option>
@@ -12,30 +24,60 @@ export default function BookingForm() {
         <option>21:00</option>
         <option>22:00</option>
       </select>
-      <label for="guests">How many dinners? </label>
+      </div>
+      <div className="guests">
+      <div className="form-title">
+        <span className="circle">3</span>
+        <label for="guests" className="res-title">How many dinners? </label>
+      </div>
       <input type="number" placeholder="1" min="1" max="10" id="guests" />
-      <p>Where would you like to sit?</p>
-      <input type="radio" id="html" name="sittingPlace" value="outside" />
-      <label for="outside">Outside</label>
-      <br />
-      <input type="radio" id="inside" name="sittingPlace" value="inside" />
-      <label for="inside">Inside</label>
-
-      <label for="occasion">Special Ocasion?</label>
+      </div>
+      <div className="sitting">
+      <div className="form-title">
+        <span className="circle">4</span>
+        <p className="res-title">Where would you like to sit?</p>
+      </div>
+      <div>
+        <input type="radio" id="html" name="sittingPlace" value="outside" />
+        <label for="outside">Outside</label>
+      </div>
+      <div>
+        <input type="radio" id="inside" name="sittingPlace" value="inside" />
+        <label for="inside">Inside</label>
+      </div>
+      </div>
+      <div className="ocasion">
+      <div className="form-title">
+         <span className="circle">5</span> 
+         <label for="occasion" className="res-title">Special ocasion?</label>
+      </div>
       <select id="occasion">
         <option>Birthday</option>
         <option>Anniversary</option>
         <option>Engagement</option>
         <option>Work party</option>
         <option>Other</option>
-        <label for="specialRequirements">Any Special Requirement?</label>
-        <input
-          type="text"
+        </select>
+        </div>
+        <div className="special-req">
+      <div className="form-title">
+        <span className="circle">6</span>
+        <label for="specialRequirements" className="res-title">Any special requirement?</label>
+      </div>
+      <textarea          
           id="specialRequirements"
           name="specialRequirements"
+          rows="10"          
         />
-      </select>
-      <input type="submit" value="Make Your reservation" />
+        </div>
+        <div className="nextbtn">
+      <Button 
+      btext={"Next"} />
+      </div>
+      <div className="backbtn">
+      <Button 
+      btext={"Back"} />
+      </div>
     </form>
   );
 }
